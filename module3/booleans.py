37a7df56ae77f7e1f818b5fc32ebffbf5869ccc0
Sherzodbek Akhmadjonov <sherzodbek.web@gmail.com>
2026-08-22 22:55:40 +0200

    Version 1.0.2: Booleans

---
+++ module3/booleans.py
@@ -0,0 +1,50 @@
+# # and
+#
+# result = 3 < 2 and 4 > 3
+# print(result)
+#
+# # or
+# result = 3 < 2 or 4 > 3
+# print(result)
+#
+# #not
+# result = not 3 < 2
+# print(result)
+
+#if/elif
+
+# my_test = 3
+#
+# if my_test == 0:
+#     print("It is 0")
+# elif my_test != 0:
+#     print("It is not 0")
+# else:
+#     print("It is not a number")
+
+#match/case
+
+# my_test = 3
+#
+# match my_test:
+#     case 0:
+#         print("It is 0")
+#     case x if x != 0:
+#         print("It is not 0")
+#     case _:
+#         print("It is not a number")
+
+#ternary operator
+
+# my_test = 3
+#
+# is_zero = "It is 0" if my_test == 0 else "It is not 0"
+#
+# print(is_zero)
+
+# x = None
+#
+# if not x:
+#     print("x is None!")
+# elif x:
+#     print("x is not None!")
