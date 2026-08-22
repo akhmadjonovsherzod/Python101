2a82f5d8b9600fd1c0ab8ae0a85e13f45f0d71c8
Sherzodbek Akhmadjonov <sherzodbek.web@gmail.com>
2026-08-22 23:12:04 +0200

    Version 1.1.0: Loops

---
+++ module3/loops.py
@@ -0,0 +1,41 @@
+#while loop
+
+# x = int(input("Enter 0 to stop: "))
+# sum_x = 0
+#
+# while x !=0:
+#     sum_x += x
+#     x = int(input("Enter 0 to stop: "))
+#
+# print(sum_x)
+
+#continue
+
+# n = int(input("Input integer number: "))
+# sum_result = 0
+# x = 1
+#
+# while x < n:
+#     x += 1
+#     if x % 2:
+#         continue
+#     sum_result += x
+#
+# print(f"Sum of even numbers: {sum_result}")
+
+#break
+
+x = int(input("Please enter a number: "))
+is_prime = True
+div = 2
+
+while div < x:
+    if not x % div:
+        is_prime = False
+        break
+    div += 1
+
+if is_prime:
+    print("Prime")
+else:
+    print("Not prime")
